d23562d293037d3184301975d71d6ff62bc0ed48
Joss-Steward <joss.steward@gmail.com>
2014-04-02 16:45:55 -0400

    Complicated

---
+++ DataParser/SRBC_DataParser_Library/DataParserLibraryTester/DataParserLibrary/ParsedData.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataParserLibrary {
+    class ParsedData {
+        public List<String[]> parsedValues;
+        public Dictionary<String, int> columnNameToIndexDict;
+        public List<String> uniqueStations;
+
+        int numberOfColumns;
+
+        public ParsedData() {
+            parsedValues = new List<string[]>();
+            columnNameToIndexDict = new Dictionary<string, int>();
+            uniqueStations = new List<string>();
+
+            numberOfColumns = 0;
+        }
+
+        //Returns the current number of unique stations
+        public int addStation(String stationName){
+            if (!uniqueStations.Contains(stationName))
+                uniqueStations.Add(stationName);
+
+            return uniqueStations.Count;
+        }
+
+        //Returns the current number of unique columns
+        public int addColumn(String columnName, int columnIndex) {
+            if (!columnNameToIndexDict.ContainsKey(columnName)) {
+                if (!columnNameToIndexDict.ContainsValue(columnIndex)) {
+                    columnNameToIndexDict.Add(columnName, columnIndex);
+                    numberOfColumns++;
+                } else { //The column index has already been added
+                    //Throw a duplicate column index error
+                }
+            } else { //The column name has already been added
+                //Throw a duplicate column name error
+            }
+
+            return columnNameToIndexDict.Count;
+        }
+
+        //Returns true if successful
+        public bool addRow(String[] row) {
+            if (row.Length != numberOfColumns)
+                return false; //Bad thing, throw exception
+
+            parsedValues.Add(row);
+
+            return true;
+        }
+
+        public String[] this[int index] {
+            get {
+                return parsedValues[index];
+            }
+        }
+    }
+}
---
+++ DataParser/SRBC_DataParser_Library/DataParserLibraryTester/DataParserLibrary/SqlUploader.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataParserLibrary
+{
+    static class SqlUploader
+    {
+    }
+}
